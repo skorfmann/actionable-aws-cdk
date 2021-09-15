661b478b2f42a30ae050b32235e5a3a562fbeaa1
Sebastian Korfmann <sebastian@korfmann.net>
2021-09-15 10:23:41 +0200

    chore: project created with projen

---
+++ .projenrc.js
@@ -0,0 +1,18 @@
+const { AwsCdkConstructLibrary } = require('projen');
+const project = new AwsCdkConstructLibrary({
+  author: 'Sebastian Korfmann',
+  authorAddress: 'sebastian@korfmann.net',
+  cdkVersion: '1.95.2',
+  defaultReleaseBranch: 'main',
+  name: 'actionable-aws-cdk',
+  repositoryUrl: 'https://github.com/sebastian/actionable-aws-cdk.git',
+
+  // cdkDependencies: undefined,      /* Which AWS CDK modules (those that start with "@aws-cdk/") does this library require when consumed? */
+  // cdkTestDependencies: undefined,  /* AWS CDK modules required for testing. */
+  // deps: [],                        /* Runtime dependencies of this module. */
+  // description: undefined,          /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],                     /* Build dependencies for this module. */
+  // packageName: undefined,          /* The "name" in package.json. */
+  // release: undefined,              /* Add release management to this project. */
+});
+project.synth();
